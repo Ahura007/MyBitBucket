0ecaafd295e0699cb90007b453ac220a6902a7a0
Ahura <Ahura@DESKTOP-NC4AC5C>
2021-02-15 09:56:58 +0330

    commit - 1

---
+++ MyBitBucket/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBitBucket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
